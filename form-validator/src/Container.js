5aceb74afb851920de278b46843f6152edca2c35
RandomMcDumDum <71903478+RandomMcDumDum@users.noreply.github.com>
2021-03-03 08:21:03 -0600

    Created a basic react-app using npx create-react-app. Got rid of any files I will not need for this project. Leaving just a bare bones project. Added style.css from the original 20vanilla projects repo as this is project is just a means of continuing to learn react and not css. Though i may choose to implement the styling on a per component basis later on. Finally added some text to the base container component to make sure the styling is working for now and we are ready to work.

---
+++ form-validator/src/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Form(){
+    return(
+        <div className='container'>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+
+export default Form;
